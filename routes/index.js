3a75a74529ba9fa8e17e4a7971ab75ff4ee1d78c
Joseph Gordy <jgordy2424@gmail.com>
2017-08-25 19:52:56 -0400

    Fixed styling on edit page

--- routes/index.js
+++ routes/index.js
@@ -57,18 +57,13 @@ router.post("/signup", function(req, res) {
     }
   }).then(function(data) {
     console.log(data);
-    res.render("listings", {users: data})
+    res.render("listings", {users: data, client: currentUser})
     // res.redirect("/");
   })
   .catch(function(err) {
     console.log(err);
     res.redirect("/");
   });
-});
-
-router.get("/user", requireLogin, function(req, res) {
-  console.log("/USERRRRRRRRRR");
-  res.render("listings", {username: req.user.username});
 });
 
 router.get("/logout", function(req, res) {
@@ -79,14 +74,15 @@ router.get("/logout", function(req, res) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 let data = []; //  variable to store data
+let currentUser;
 
 router.get("/", requireLogin, function(req, res) {
 
   User.find({}).sort("name")
   .then(function(users) {
-    // console.log(users);
+    currentUser = req.user;
     data = users;
-    res.render("listings", {users: data})
+    res.render("listings", {users: data, client: currentUser})
   })
   .catch(function(err) {
     console.log(err);
@@ -103,7 +99,7 @@ router.get('/looking', function (req, res) {
   User.find({job: null}).sort("name")
   .then(function(users) {
     data = users;
-    res.render("listings", {users: data})
+    res.render("listings", {users: data, client: currentUser})
   })
   .catch(function(err) {
     console.log(err);
@@ -121,7 +117,7 @@ router.get('/employed', function(req, res) {
   User.find({job: {$not: {$type: 10}}}).sort("name")
   .then(function(users) {
     data = users;
-    res.render("listings", {users: data})
+    res.render("listings", {users: data, client: currentUser})
   })
   .catch(function(err) {
     console.log(err);
@@ -132,18 +128,98 @@ router.get('/employed', function(req, res) {
 
 /////////////////////////////////////////////////////////////////////////////
 router.get('/listing/:id', function (req, res) {
+  if (req.user.id == req.params.id) {
 
-  User.find({_id: req.params.id}).sort("name")
-  .then(function(users) {
-    // console.log(users);
-    data = users;
-    res.render("singleListing", {users: data})
-  })
-  .catch(function(err) {
-    console.log(err);
-    next(err);
-  });
+    User.find({_id: req.params.id}).sort("name")
+    .then(function(users) {
+      // console.log(users);
+      data = users;
+      res.render("edit_profile", {users: data, client: currentUser})
+    })
+    .catch(function(err) {
+      console.log(err);
+      // next(err);
+    });
+
+  } else {
+
+    User.find({_id: req.params.id}).sort("name")
+    .then(function(users) {
+      // console.log(users);
+      data = users;
+      res.render("singleListing", {users: data, client: currentUser})
+    })
+    .catch(function(err) {
+      console.log(err);
+      // next(err);
+    });
+
+  }
 
 });
+
+
+router.post("/save/:id", function(req, res) {
+  let tempId = req.params.id
+  let profileData = {};
+
+    if (req.body.name) {
+      profileData.name = req.body.name;
+    };
+    if (req.body.email) {
+      profileData.email = req.body.email;
+    };
+    if (req.body.username) {
+      profileData.username = req.body.username;
+    };
+    if (req.body.university) {
+      profileData.university = req.body.university;
+    };
+    if (req.body.company) {
+      profileData.company = req.body.company;
+    };
+    if (req.body.job) {
+      profileData.job = req.body.job;
+    };
+    if (req.body.skills) {
+      profileData.skills = req.body.skills;
+    };
+    if (req.body.phone) {
+      profileData.phone = req.body.phone;
+    };
+    if (req.body.streetNum) {
+      profileData.street_num = {"address": req.body.streetNum};
+    };
+    if (req.body.streetName) {
+      profileData.street_name = {"address": req.body.streetName};
+    };
+    if (req.body.city) {
+      profileData.city = {"address": req.body.city};
+    };
+    if (req.body.state) {
+      profileData.state_or_province = {"address": req.body.state};
+    };
+    if (req.body.postal) {
+      profileData.postal_code = {"address": req.body.postal};
+    };
+    if (req.body.country) {
+      profileData.country = {"address": req.body.country};
+    };
+    if (req.body.director) {
+      profileData.director = req.body.director;
+    };
+
+    User.update({_id: tempId},
+      {$set: profileData})
+    .then(function(data) {
+      console.log("update: ", data);
+      res.redirect("/listing/" + tempId)
+    })
+    .catch(function(err) {
+      console.log("update: ", err);
+
+    })
+
+})
 
 module.exports = router;
